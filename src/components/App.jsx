1264aeb1b27fe6ec671a55abee17cc8d654426d0
Prince-Attrish <vs3031999@gmail.com>
2023-07-25 12:22:35 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import { CssBaseline } from "@mui/material";
+// import { Route } from "react-router-dom";
+
+const App = () => (
+    <div>
+        <CssBaseline />
+        <h1>Hello, World - FilmFire</h1>
+    </div>
+);
+
+export default App
